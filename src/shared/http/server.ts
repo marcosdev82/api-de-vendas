02f3b36a9b8039f19adbd30ccd5df123b222f5ce
mikek182 <marcosvinicius@geekwork.com.br>
2024-07-20 16:12:20 -0300

    Modificação da porta do express de 3333 para 3000

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 
+
 const app = express();
 
 app.use(cors());
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log('Server started on port 3333!')
+// a porta 3333 não funcionou
+app.listen(3000, () => {
+  console.log('Server started on port 3000!');
 });
