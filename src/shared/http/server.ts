02e8ec2344a5ced8c1090a261e9fddc8889f6ed8
mikek182 <marcosvinicius@geekwork.com.br>
2024-07-20 16:27:31 -0300

    Criação da classe AppError

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,14 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use(routes);
-
 // a porta 3333 não funcionou
 // http://localhost:3000/
 app.listen(3000, () => {
