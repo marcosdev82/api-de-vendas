dd2dc54346571bb0e193abdcd453471f63a00d2d
mikek182 <marcosvinicius@geekwork.com.br>
2024-07-25 12:54:30 -0300

    Removendo interface da ListProductService

--- src/modules/products/services/ShowProductService.ts
+++ src/modules/products/services/ShowProductService.ts
@@ -2,8 +2,14 @@ import { getCustomRepository } from "typeorm";
 import { ProductRepository } from "../typeorm/repositories/ProductsRepository";
 import Product from "../typeorm/entities/Product";
 
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
 class ListProductService {
-  public async execute(): Promise<Product[]>{
+  public async execute(): Promise<Product>{
     const productsRepository = getCustomRepository(ProductRepository);
 
     const products =  productsRepository.find();
