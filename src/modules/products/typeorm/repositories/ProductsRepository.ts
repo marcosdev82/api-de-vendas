78e3bdc8e5b135d70075d2916b37d1d5fedb80d5
mikek182 <marcosvinicius@geekwork.com.br>
2024-07-25 17:11:19 -0300

    modificação da versão do tsconfig-paths

--- src/modules/products/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -1,16 +1,16 @@
-import { EntityRepository, Repository} from 'typeorm';
+import { EntityRepository, Repository } from 'typeorm';
 import Product from '../entities/Product';
 
 @EntityRepository(Product)
-export class ProductRepository extends Repository<Product> {
-  public async findByName(name: string): Promise<Product | undefined> {
-    const product = this.findOne({
+export default class ProductRepository extends Repository<Product> {
+  public async findByName(name: string): Promise<Product | null> {
+    // Usar await para garantir que a chamada ao banco de dados seja concluída
+    const product = await this.findOne({
       where: {
         name,
       },
-    })
+    });
 
     return product;
   }
 }
-
