f168c68471a2f6a9db0e8138d762ba5946b4b874
mikek182 <marcosvinicius@geekwork.com.br>
2024-07-26 17:57:03 -0300

    Criação do método update produc

--- src/modules/products/controllers/ProductsController.ts
+++ src/modules/products/controllers/ProductsController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import ListProductService from "../services/ListProductService";
 import ShowProductService from "../services/ShowProductService";
 import CreateProductService from "../services/CreateProductService";
+import UpdateProductService from "../services/UpdateProductService";
 
 export default class ProductsController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -28,6 +29,22 @@ export default class ProductsController {
     const createProduct = new CreateProductService();
 
     const product = await createProduct.execute({
+      name,
+      price,
+      quantity
+    });
+
+    return response.json(product)
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { name, price, quantity} = request.body;
+    const { id } = request.params;
+
+    const updateProduct = new UpdateProductService();
+
+    const product = updateProduct.execute({
+      id,
       name,
       price,
       quantity
