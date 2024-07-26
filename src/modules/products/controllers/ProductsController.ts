fbc90420225429808ed2ae735854f257095d7860
mikek182 <marcosvinicius@geekwork.com.br>
2024-07-26 13:05:07 -0300

    Criação da ProductsController e do método index para lista de produtos

---
+++ src/modules/products/controllers/ProductsController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import ListProductService from "../services/ListProductService";
+
+export default class ProductsController {
+  public async index(request: Request, response: Response): Promise<> {
+    const listProducts = new ListProductService();
+
+    const product = listProducts.execute()
+
+    return response.json(product)
+  }
+
+
+}
