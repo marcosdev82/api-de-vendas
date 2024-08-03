04455c58922c6a8e58b9001bc9f23aeb567f26e1
mikek182 <marcosvinicius@geekwork.com.br>
2024-08-03 13:26:55 -0300

    Crição do serviço de sessão de usuários

---
+++ src/modules/users/services/CreateSessionService.ts
@@ -0,0 +1,36 @@
+import { getCustomRepository } from "typeorm";
+import AppError from "@shared/errors/AppError";
+import User from "../typeorm/entities/Users";
+import UsersRepository from "../typeorm/repositories/UsersRepository";
+import { compare, hash } from "bcryptjs";
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+// interface IResponse {
+//   user: User;
+// }
+
+class CreateSessionService {
+  public async execute({ email, password}: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await  usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    const hashedPassword = await compare(password, user.password);
+
+    if (!hashedPassword) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    return user;
+  }
+}
+
+export default CreateSessionService;
