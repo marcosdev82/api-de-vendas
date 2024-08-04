f863b39795f5b1fb7d2968f69719e6206e66e132
mikek182 <marcosvinicius@geekwork.com.br>
2024-08-04 11:58:11 -0300

    Middleware que autentica JWT

---
+++ src/modules/users/middleware/isAuthenticated.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+
+export default function isAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+
+}
