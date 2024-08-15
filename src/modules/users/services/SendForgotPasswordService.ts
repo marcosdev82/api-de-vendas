762460c6ebfa0cece96ca1067dcc370ad6e4ce1a
mikek182 <marcosvinicius@geekwork.com.br>
2024-08-15 16:59:25 -0300

    Inportação e implementação do sendMail

--- src/modules/users/services/SendForgotPasswordService.ts
+++ src/modules/users/services/SendForgotPasswordService.ts
@@ -2,6 +2,8 @@ import { getCustomRepository } from "typeorm";
 import AppError from "@shared/errors/AppError";
 import UsersRepository from "../typeorm/repositories/UsersRepository";
 import UserTokensRepository from "../typeorm/repositories/UserTokensRepository";
+import EtherealMail from "@config/mail/EtherealMail";
+import Mail from "nodemailer/lib/mailer";
 
 interface IRequest {
   email: string;
@@ -21,6 +23,11 @@ class SendForgotPasswordService {
     const token  = await userTokensRepository.generate(user.id);
 
     console.log(token);
+
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida: ${token}`
+    })
   }
 }
 
