28e2451ee9fe68b1109c08da372e5d1e06c13af7
mikek182 <marcosvinicius@geekwork.com.br>
2024-08-17 11:18:55 -0300

    Template Handlebars para email

---
+++ src/config/mail/HandlebarsMailTemplate.ts
@@ -0,0 +1,19 @@
+import Handlebars from "handlebars";
+
+
+interface ITemplateVariable {
+  [key: string]: string | number;
+}
+
+interface IParseEmailTemplate {
+  template: string;
+  variables: ITemplateVariable;
+}
+
+export default class HandlebarsMailTemplate {
+  public async parse({template, variables}: IParseEmailTemplate): Promise<string> {
+    const parseTemplate = Handlebars.compile(template);
+
+    return parseTemplate(variables);
+  }
+}
