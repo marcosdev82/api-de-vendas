674f0fd47863afeb5a7d3988200f31e6873ce7e1
mikek182 <marcosvinicius@geekwork.com.br>
2024-08-25 11:30:49 -0300

    Serviço de atualização de cliente

---
+++ src/modules/customers/services/UpdateCustomerService.ts
@@ -0,0 +1,44 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+import { compare, hash } from 'bcryptjs';
+import CustomersRepository from '../typeorm/repositories/CustomersRepository';
+import Customer from '../typeorm/entities/Customer';
+
+interface IRequest {
+  id: string;
+  name: string;
+  email: string;
+}
+
+class UpdateCustomerService {
+  public async execute({
+    id,
+    name,
+    email,
+  }: IRequest): Promise<Customer> {
+    const custumersRepository = getCustomRepository(CustomersRepository);
+
+    const customer = await custumersRepository.findById(id);
+
+    if (!customer) {
+      throw new AppError('Customer not found.');
+    }
+
+    const customerExists = await customersRepository.findByEmail(email);
+
+    if (customerExists && email !== customer.email) {
+      throw new AppError('There is already one customer with this email.');
+    }
+
+
+
+    customer.name = name;
+    customer.email = email;
+
+    await custumersRepository.save(customer)
+
+    return customer;
+  }
+}
+
+export default UpdateCustomerService;
