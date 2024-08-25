db5d3d30429369756da205f11a82aabfe6066014
mikek182 <marcosvinicius@geekwork.com.br>
2024-08-25 13:07:52 -0300

    Rota de clientes

---
+++ src/modules/customers/routes/customers.routes.ts
@@ -0,0 +1,51 @@
+import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+import CustomersController from '../controller/CustomersController';
+
+const customersRouter = Router();
+const customersController = new CustomersController();
+
+customersRouter.get('/', customersController.index);
+customersRouter.get(
+   '/:id',
+   celebrate({
+      [Segments.PARAMS]: {
+        id: Joi.string().uuid().required(),
+      }
+   }),
+   customersController.show
+);
+customersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required()
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    }
+  }),
+  customersController.create
+);
+customersRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required()
+    }
+  }),
+  customersController.update
+);
+customersRouter.delete(
+  '/:id',
+  celebrate({
+     [Segments.PARAMS]: {
+       id: Joi.string().uuid().required(),
+     }
+  }),
+  customersController.delete
+);
+
+export default customersRouter;
