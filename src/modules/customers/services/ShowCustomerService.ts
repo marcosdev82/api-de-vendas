c5fbc44bf4388a02711a1e0cf99cbd0278f60afc
mikek182 <marcosvinicius@geekwork.com.br>
2024-08-25 11:19:54 -0300

    Serviço que retorna cliente por ID

---
+++ src/modules/customers/services/ShowCustomerService.ts
@@ -0,0 +1,24 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+import CustomersRepository from '../typeorm/repositories/CustomersRepository';
+import Customer from '../typeorm/entities/Customer';
+
+interface IRequest {
+  id: string;
+}
+
+class ShowCustomerService {
+  public async execute({id}: IRequest): Promise<Customer> {
+    const customerRepository = getCustomRepository(CustomersRepository);
+
+    const customer = await customerRepository.findById(id);
+
+    if (!customer) {
+      throw new AppError('Customer not found.');
+    }
+
+    return customer;
+  }
+}
+
+export default ShowCustomerService;
