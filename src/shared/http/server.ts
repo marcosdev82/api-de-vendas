8a653838fb6d4f06fa1f531226afc6365fea66f8
mikek182 <marcosvinicius@geekwork.com.br>
2024-09-06 13:03:05 -0300

    Adicionando a middle de paginação na server ts

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,12 +6,15 @@ import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
 import { errors } from 'celebrate';
+import {pagination} from 'typeorm-pagination';
 import uploadConfig from '@config/upload';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(pagination);
+
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
